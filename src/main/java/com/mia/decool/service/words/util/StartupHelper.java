17fec75d70b18ec47487b31562f99706f6ef73f2
miajoskowicz <miajoskowicz@pingidentity.com>
2023-08-19 21:15:03 +0300

    Making the app startup log nicer.

--- src/main/java/com/mia/decool/service/words/util/StartupHelper.java
+++ src/main/java/com/mia/decool/service/words/util/StartupHelper.java
@@ -1,11 +1,7 @@
 package com.mia.decool.service.words.util;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.core.env.Environment;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 
 @Slf4j
@@ -14,27 +10,17 @@ public final class StartupHelper {
     public static void logApplicationStartup(Environment env) {
         String protocol = "http";
         String serverPort = env.getProperty("server.port");
-        String contextPath = "/";
-        String hostAddress = "localhost";
-        try {
-            hostAddress = InetAddress.getLocalHost().getHostAddress();
-        } catch (UnknownHostException e) {
-            log.warn("The host name could not be determined, using `localhost` as fallback");
-        }
         log.info("""
 
                         ----------------------------------------------------------
                         \tApplication '{}' is running! Access URLs:
-                        \tLocal: \t\t{}://localhost:{}{}
-                        \tExternal: \t{}://{}:{}{}
+                        \tBase: \t\t{}://localhost:{}/
+                        \tSwagger: \t{}://localhost:{}/swagger-ui.html
                         ----------------------------------------------------------""",
                 env.getProperty("spring.application.name"),
                 protocol,
                 serverPort,
-                contextPath,
                 protocol,
-                hostAddress,
-                serverPort,
-                contextPath);
+                serverPort);
     }
 }
